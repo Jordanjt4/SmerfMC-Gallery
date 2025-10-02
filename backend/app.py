8137e765bffaec964917995ed6b418c5bf78f545
jordanjt4 <jordan.jt4@comcast.net>
2025-10-02 02:15:43 -0700

    Updated API to pass by query instead of path

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import os
 import boto3
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from dotenv import load_dotenv
 from botocore.exceptions import ClientError
@@ -10,11 +10,15 @@ load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
+# Initializing supabase to allow us to interact with the database
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+# Initializing s3 to allow us to interact with objects
 s3 = boto3.client('s3')
 
 app = Flask(__name__)
-CORS(app)
+# Enable requests from any origin to any route
+CORS(app) 
 
 # Generate url used to display image. Expires in an hour
 def create_presigned_url(bucket_name, key, expiration=3600):
@@ -28,43 +32,50 @@ def create_presigned_url(bucket_name, key, expiration=3600):
     except ClientError as e:
         print(e)
 
-def get_categories():
+# Return list of all categories
+def get_categories() -> list[str]:
     rows = supabase.table("categories").select("categoryName").order("categoryName").execute()
     data = rows.data
     names = [d['categoryName'] for d in data]
-    print(names)
     return names
 
-# Given a category, returns a description
+# Given a category, returns its description
 def get_description(category: str):
-    print(category)
     rows = supabase.table("categories").select("categoryDescription").eq("categoryName", category).execute()
     data = rows.data
     description = data[0]['categoryDescription']
     return description
 
-# Given a category, returns a list of urls
+# Given a category, returns a list of generated presigned urls
 def get_images(category: str):
     rows = supabase.table("images").select("object_key", "caption").eq("category", category).execute()
     data = rows.data
+    # List of dictionaries with image urls and corresponding caption
     urls = [{"url": create_presigned_url("smerfmc", d["object_key"]), "caption": d["caption"]} for d in data]
     return urls
 
+# Initial load to the homepage needs all the categories for the dropdown menu
 @app.get("/api/categories")
 def list_categories():
     categories = get_categories()
     return jsonify(categories)
 
-@app.get("/api/category-description/<category>")
-def cat_description(category):
+@app.get("/api/category-description")
+def cat_description():
+    category = request.args.get("category")
+    if not category:
+        return jsonify({"description" : ""})
     description = get_description(category)
     if description:
         return jsonify({"description" : description})
     else:
         return jsonify({"description" : ""})
 
-@app.get("/api/category-images/<category>")
-def cat_images(category):
+@app.get("/api/category-images")
+def cat_images():
+    category = request.args.get("category")
+    if not category:
+        return jsonify([])
     urls = get_images(category)
     if urls:
         return jsonify(urls)
